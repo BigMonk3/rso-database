173de1b3fac6fd7787d5bc4758dd90b8a5f5ea35
julian saenz <juju@julians-MacBook-Pro-5.local>
2025-04-15 13:54:47 -0600

    updated mission control class

---
+++ SpaceDebrisSystem/MissionControl.java
@@ -0,0 +1,8 @@
+/*
+ * manage user roles, menu, and actions
+ */
+public class MissionControl {
+    public void start(){
+       System.out.println("Welcome to the Space Debris Monitoring System");
+    }   
+}
