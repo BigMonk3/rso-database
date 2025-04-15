4e0ac070ace11e3a48aa9b8a45d4a5bf955e0366
julian saenz <juju@julians-MacBook-Pro-5.local>
2025-04-15 13:56:00 -0600

    updated run simulation class

---
+++ SpaceDebrisSystem/RunSimulation.java
@@ -0,0 +1,9 @@
+/*
+ * main class that starts program
+ */
+public class RunSimulation{
+    public static void main(String[] args){
+        MissionControl control = new MissionControl();
+        control.start();
+    }
+}
