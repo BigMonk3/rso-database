ce6d2656296f92db2c493eeaf3f4ed1e86af4a6f
Jesussaenz27 <juju@julians-MBP-5.attlocal.net>
2025-04-27 23:20:13 -0600

    added comments to logger class

--- SpaceDebrisSystem/Logger.java
+++ SpaceDebrisSystem/Logger.java
@@ -5,18 +5,18 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * logger class that appends timestamped log messages to a file
- * can be used across the system to record actions performed by users
+ * logger class that appends interaction with system to a file
+ * used across the system to log actions performed by users
  */
 public class Logger {
-                                                //enter a file path to have logger saved 
+
     private static final String LOG_FILE_PATH = "data/logfile";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     /**
-     * logs a message with a timestamp and appends it to the log file
+     * logs message with timestamp and appends it to the log file
      *
-     * @param message The message to log without timestamp
+     * @param message to log without timestamp
      */
     public static void log(String message) {
         String timestamp = LocalDateTime.now().format(formatter);
@@ -31,14 +31,14 @@ public class Logger {
     }
 
     /**
-     * log for program exit behavior.
+     * logs when program is exited
      */
     public static void logExit() {
         log("Program exited by user. Final debris report and logs have been saved.");
     }
 
     /**
-     * optional method to log a blank line for visual separation.
+     * method to log a blank line for visual separation
      */
     public static void newSection() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE_PATH, true))) {
